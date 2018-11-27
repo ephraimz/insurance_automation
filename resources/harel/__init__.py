3728b80ccf622c58751394ca5a7f63f6852ae288
Arseny <arseny@ezsave.co.il>
2018-11-27 15:00:57 +0200

    Update auth

--- resources/harel/__init__.py
+++ resources/harel/__init__.py
@@ -4,18 +4,30 @@ from bs4 import BeautifulSoup
 from requests.utils import cookiejar_from_dict
 
 
-VALIDATE_USER_URL = ('https://www.harel-group.co.il/_vti_bin/webapi/'
-                     'CustomersAuthentication/PostAuthenticate/ValidateUser')
+AUTH_URL = ('https://www.harel-group.co.il/_vti_bin/webapi/'
+            'CustomersAuthentication/PostAuthenticate/ValidateUser')
+AUTH_CONFIRM_URL = ('https://www.harel-group.co.il/_vti_bin/webapi/'
+                    'CustomersAuthentication/PostAuthenticate/ValidateOTP')
 AUTH_COOKIE_NAME = 'ASP.NET_SessionId'
 
 
 class Harel:
     def authenticate(self, user_id, phone):
         self.session = requests.Session()
-        r = self.session.post(VALIDATE_USER_URL, data={
-            "UserId": user_id,
-            "FullPhone": phone,
+        r = self.session.post(AUTH_URL, data={
+            'UserId': user_id,
+            'FullPhone': phone,
         })
+        response_data = r.json()
+        return response_data['Status'] == 0
+
+    def confirm_authentication(self, code):
+        r = self.session.post(AUTH_CONFIRM_URL, data={
+            'OTP': code,
+            'captchaResponse': None
+        })
+        response_data = r.json()
+        return response_data['Status'] == 0
 
     @property
     def session_id(self):
