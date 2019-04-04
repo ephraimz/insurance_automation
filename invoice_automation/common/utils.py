b604c959b10c8f61cffa16e94d0567a97ed92078
aruseni <aruseni.magiku@gmail.com>
2019-04-04 12:17:53 +0300

    Update deep_get

--- invoice_automation/common/utils.py
+++ invoice_automation/common/utils.py
@@ -1,13 +1,11 @@
-from functools import reduce
-
-
 def deep_get(dictionary, keys, default=None):
-    return reduce(
-        lambda d, key:
-        d.get(key, default) if isinstance(d, dict) else default,
-        keys.split("."),
-        dictionary
-    )
+    v = dictionary
+    for k in keys.split('.'):
+        if isinstance(v, dict) and k in v:
+            v = v[k]
+        else:
+            return default
+    return v
 
 
 def has_all_needed_keys(dictionary, keys):
